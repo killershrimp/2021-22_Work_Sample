9b5452c7f5ab477d696dcffa362b2a0dad445488
SumiGovindaraju <sumi.govindaraju20@bcp.org>
2020-01-04 09:43:50 -0800

    Add some lib code + CTRE dep

---
+++ src/main/java/com/team254/frc2020/Constants.java
@@ -0,0 +1,49 @@
+package com.team254.frc2020;
+
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+/**
+ * A list of constants used by the rest of the robot code. This includes physics
+ * constants as well as constants determined through calibration.
+ */
+public class Constants {
+    public static final double kLooperDt = 0.01;
+
+    public static final int kCANTimeoutMs = 10; // use for important on the fly updates
+    public static final int kLongCANTimeoutMs = 100; // use for constructors
+
+    // Control board
+    public static final double kJoystickThreshold = 0.2;
+
+    /**
+     * @return the MAC address of the robot
+     */
+    public static String getMACAddress() {
+        try {
+            Enumeration<NetworkInterface> nwInterface = NetworkInterface.getNetworkInterfaces();
+            StringBuilder ret = new StringBuilder();
+            while (nwInterface.hasMoreElements()) {
+                NetworkInterface nis = nwInterface.nextElement();
+                if (nis != null) {
+                    byte[] mac = nis.getHardwareAddress();
+                    if (mac != null) {
+                        for (int i = 0; i < mac.length; i++) {
+                            ret.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+                        }
+                        return ret.toString();
+                    } else {
+                        System.out.println("Address doesn't exist or is not accessible");
+                    }
+                } else {
+                    System.out.println("Network Interface for the specified address is not found.");
+                }
+            }
+        } catch (SocketException | NullPointerException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+}
