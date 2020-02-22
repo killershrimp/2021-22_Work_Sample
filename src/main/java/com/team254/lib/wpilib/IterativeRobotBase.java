101ae2063af60464988b8d048bf3d12a1cc93ab9
SumiGovindaraju <sumi.govindaraju20@bcp.org>
2020-02-21 19:57:23 -0800

    update phoenix and header files

--- src/main/java/com/team254/lib/wpilib/IterativeRobotBase.java
+++ src/main/java/com/team254/lib/wpilib/IterativeRobotBase.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2017-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -62,6 +62,7 @@ public abstract class IterativeRobotBase extends RobotBase {
 	protected IterativeRobotBase(double period) {
 		m_period = period;
 		m_watchdog = new Watchdog(period, this::printLoopOverrunMessage);
+		m_watchdog.suppressTimeoutMessage(true);
 	}
 
 	/**
@@ -88,6 +89,19 @@ public abstract class IterativeRobotBase extends RobotBase {
 	 */
 	public void robotInit() {
 		System.out.println("Default robotInit() method... Override me!");
+	}
+
+	/**
+	 * Robot-wide simulation initialization code should go here.
+	 *
+	 * <p>
+	 * Users should override this method for default Robot-wide simulation related
+	 * initialization which will be called when the robot is first started. It will
+	 * be called exactly one time after RobotInit is called only when the robot is
+	 * in simulation.
+	 */
+	public void simulationInit() {
+		System.out.println("Default simulationInit() method... Override me!");
 	}
 
 	/**
@@ -149,6 +163,21 @@ public abstract class IterativeRobotBase extends RobotBase {
 		}
 	}
 
+	private boolean m_spFirstRun = true;
+
+	/**
+	 * Periodic simulation code should go here.
+	 *
+	 * <p>
+	 * This function is called in a simulated robot after user code executes.
+	 */
+	public void simulationPeriodic() {
+		if (m_spFirstRun) {
+			System.out.println("Default simulationPeriodic() method... Override me!");
+			m_spFirstRun = false;
+		}
+	}
+
 	private boolean m_dpFirstRun = true;
 
 	/**
@@ -198,6 +227,7 @@ public abstract class IterativeRobotBase extends RobotBase {
 		}
 	}
 
+	@SuppressWarnings("PMD.CyclomaticComplexity")
 	protected void loopFunc() {
 		m_watchdog.reset();
 
@@ -273,6 +303,12 @@ public abstract class IterativeRobotBase extends RobotBase {
 		m_watchdog.addEpoch("LiveWindow.updateValues()");
 		Shuffleboard.update();
 		m_watchdog.addEpoch("Shuffleboard.update()");
+
+		if (isSimulation()) {
+			simulationPeriodic();
+			m_watchdog.addEpoch("simulationPeriodic()");
+		}
+
 		m_watchdog.disable();
 
 		// Warn on loop time overruns
