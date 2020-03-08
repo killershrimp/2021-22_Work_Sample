49413bc0c4daadd9cd090d165cb00240dea51bda
Alex Yun <alexyun04@gmail.com>
2020-03-07 21:37:10 -0800

    add spare LL constants

--- src/main/java/com/team254/frc2020/limelight/constants/LimelightConstantsFactory.java
+++ src/main/java/com/team254/frc2020/limelight/constants/LimelightConstantsFactory.java
@@ -10,8 +10,8 @@ import com.team254.lib.geometry.Rotation2d;
 
 public class LimelightConstantsFactory {
     public static LimelightConstants getConstantsForId(int id) {
-        double kLensOffGroundHeight = 26.48;
-        Rotation2d kHorizontalPlaneToLens = Rotation2d.fromDegrees(27.00);
+        double kTurretLimelightLensOffGroundHeight = 26.48;
+        Rotation2d kTurretLimelightHorizontalPlaneToLens = Rotation2d.fromDegrees(27.00);
 
         switch (id) {
             default: // Intentional fall through
@@ -45,10 +45,10 @@ public class LimelightConstantsFactory {
                         LimelightConstants.Type.Shooter,
                         "Turret Limelight #1", // name
                         "limelight", // table name
-                        kLensOffGroundHeight, // height
+                        kTurretLimelightLensOffGroundHeight, // height
                         new Pose2d(-5.7, 0, Rotation2d.fromDegrees(1.0)), // turret to lens
-                        kHorizontalPlaneToLens, // horizontalPlaneToLens,
-                        new UndistortMap_Limelight_1_320x240(), //undistort map
+                        kTurretLimelightHorizontalPlaneToLens, // horizontalPlaneToLens,
+                        new UndistortMap_Limelight_1_320x240(), // undistort map
                         new UndistortConstants( // undistort constants
                                 new double[]{2.03204609e-01, -6.25404962e-01, -3.39277869e-03, -3.51126715e-04, 5.81122457e-01}, // camera distortion
                                 new double[][]{ // camera matrix
@@ -67,9 +67,9 @@ public class LimelightConstantsFactory {
                         LimelightConstants.Type.Shooter,
                         "Turret Limelight #2", // name
                         "limelight", // table name
-                        kLensOffGroundHeight, // height
+                        kTurretLimelightLensOffGroundHeight, // height
                         new Pose2d(-5.7, 0, Rotation2d.fromDegrees(1.5)), // turret to lens
-                        kHorizontalPlaneToLens, // horizontalPlaneToLens,
+                        kTurretLimelightHorizontalPlaneToLens, // horizontalPlaneToLens,
                         new UndistortMap_Limelight_2_320x240(),
                         new UndistortConstants( // undistort constants
                                 new double[]{2.08955661e-01, -6.41863833e-01, 6.17627447e-04, -6.37834053e-05, 6.35575213e-01}, // camera distortion
@@ -80,6 +80,94 @@ public class LimelightConstantsFactory {
                                 }),
                         64.03840065743408,
                         50.34836606499798
+                );
+
+            case 3:
+                // Calibrated 3/2
+                return new LimelightConstants(
+                        3, // label id
+                        LimelightConstants.Type.StationAlignment,
+                        "Intake Limelight #3", // name
+                        "limelight", // table name
+                        kTurretLimelightLensOffGroundHeight, // height TODO set to intake LL constants
+                        new Pose2d(-5.7, 0, Rotation2d.fromDegrees(1.5)), // turret to lens TODO set to intake LL constants
+                        kTurretLimelightHorizontalPlaneToLens, // horizontalPlaneToLens TODO set to intake LL constants
+                        null,   // TODO generate map
+                        new UndistortConstants( // undistort constants
+                                new double[]{0.20120696, -0.54084708, 0.0014172, -0.00512332, 0.46871865}, // camera distortion
+                                new double[][]{ // camera matrix
+                                        {0.8027808, 0.0, 0.47932945},
+                                        {0.0, 1.07363039, 0.51292353},
+                                        {0.0, 0.0, 1.0}
+                                }),
+                        63.807633432913114,
+                        49.93847526720772
+                );
+
+            case 4:
+                // TODO calibrate
+                return new LimelightConstants(
+                        4, // label id
+                        LimelightConstants.Type.StationAlignment,
+                        "Intake Limelight #4", // name
+                        "limelight", // table name
+                        kTurretLimelightLensOffGroundHeight, // height TODO set to intake LL constants
+                        new Pose2d(-5.7, 0, Rotation2d.fromDegrees(1.5)), // turret to lens TODO set to intake LL constants
+                        kTurretLimelightHorizontalPlaneToLens, // horizontalPlaneToLens TODO set to intake LL constants
+                        null,   // TODO generate map
+                        new UndistortConstants( // undistort constants
+                                new double[]{2.08955661e-01, -6.41863833e-01, 6.17627447e-04, -6.37834053e-05, 6.35575213e-01}, // camera distortion
+                                new double[][]{ // camera matrix
+                                        {0.79943057, 0.0, 0.51249286},
+                                        {0.0, 1.06369973, 0.51129839},
+                                        {0.0, 0.0, 1.0}
+                                }),
+                        64.03840065743408,
+                        50.34836606499798
+                );
+
+            case 5:
+                // Calibrated 3/2
+                return new LimelightConstants(
+                        5, // label id
+                        LimelightConstants.Type.Shooter,
+                        "Limelight #5", // name
+                        "limelight", // table name
+                        kTurretLimelightLensOffGroundHeight, // height
+                        new Pose2d(-5.7, 0, Rotation2d.fromDegrees(1.5)), // turret to lens
+                        kTurretLimelightHorizontalPlaneToLens, // horizontalPlaneToLens,
+                        null,   // TODO generate map
+                        new UndistortConstants( // undistort constants
+                                new double[]{0.20014465, -0.47922169, -0.00415085, -0.00084271, 0.35440957}, // camera distortion
+                                new double[][]{ // camera matrix
+                                        {0.82360559, 0.0, 0.47004554},
+                                        {0.0, 1.09854666, 0.45221907},
+                                        {0.0, 0.0, 1.0}
+                                }),
+                        62.473651304991805,
+                        48.877367093835794
+                );
+
+            case 6:
+                // Calibrated 3/2
+                return new LimelightConstants(
+                        6, // label id
+                        LimelightConstants.Type.Shooter,
+                        "Limelight #6", // name
+                        "limelight", // table name
+                        kTurretLimelightLensOffGroundHeight, // height
+                        new Pose2d(-5.7, 0, Rotation2d.fromDegrees(1.5)), // turret to lens
+                        kTurretLimelightHorizontalPlaneToLens, // horizontalPlaneToLens
+                        null,   // TODO generate map
+                        new UndistortConstants( // undistort constants
+                                new double[]{1.90847215e-01, -5.13780075e-01, -2.34747538e-04, -2.56021324e-03, 4.27250411e-01}, // camera distortion
+                                new double[][]{ // camera matrix
+                                        {0.81140397, 0.0, 0.46869083},
+                                        {0.0, 1.08436796, 0.48825234},
+                                        {0.0, 0.0, 1.0}
+                                }),
+                        63.228865885979154,
+                        49.50445641798894
                 );
         }
     }
