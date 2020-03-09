75be415b763864aeff9a95d7f5c5a0987307a9c2
Solomon Chang <changsolomon8@gmail.com>
2020-03-09 13:57:33 -0700

    Add missing files

---
+++ src/main/java/com/team254/frc2020/subsystems/utils/TimedLEDState.java
@@ -0,0 +1,60 @@
+package com.team254.frc2020.subsystems.utils;
+
+public interface TimedLEDState {
+    void getCurrentLEDState(LEDState desiredState, double timestamp);
+
+    class BlinkingLEDState implements TimedLEDState {
+        public static BlinkingLEDState kZeroingFault = new BlinkingLEDState(
+                LEDState.kOff, LEDState.kFault, 1.0);
+        public static BlinkingLEDState kZeroingHoodFault = new BlinkingLEDState(
+                LEDState.kOff, LEDState.kFaultHood, 1.0);
+
+        public static BlinkingLEDState kClimbing = new BlinkingLEDState(
+                LEDState.kOff, LEDState.kClimbing, 0.5);
+         public static BlinkingLEDState kBreakEngaged = new BlinkingLEDState(
+                LEDState.kOff, LEDState.kBreakEngaged, 0.5);
+
+
+        public static BlinkingLEDState kBlinkingAiming = new BlinkingLEDState(
+                LEDState.kOff, LEDState.kAiming, 0.1);
+        public static BlinkingLEDState kBlinkingShooting = new BlinkingLEDState(
+                LEDState.kOff, LEDState.kShooting, 0.1);
+
+
+        LEDState mStateOne = new LEDState(0.0, 0.0, 0.0);
+        LEDState mStateTwo = new LEDState(0.0, 0.0, 0.0);
+        double mDuration;
+
+        public BlinkingLEDState(LEDState stateOne, LEDState stateTwo, double duration) {
+            mStateOne.copyFrom(stateOne);
+            mStateTwo.copyFrom(stateTwo);
+            mDuration = duration;
+        }
+
+        @Override
+        public void getCurrentLEDState(LEDState desiredState, double timestamp) {
+            if ((int) (timestamp / mDuration) % 2 == 0) {
+                desiredState.copyFrom(mStateOne);
+            } else {
+                desiredState.copyFrom(mStateTwo);
+            }
+        }
+    }
+
+    class StaticLEDState implements TimedLEDState {
+        public static StaticLEDState kStaticOff = new StaticLEDState(LEDState.kOff);
+        public static StaticLEDState kRobotZeroed = new StaticLEDState(LEDState.kRobotZeroed);
+
+
+        LEDState mStaticState = new LEDState(0.0, 0.0, 0.0);
+
+        public StaticLEDState(LEDState staticState) {
+            mStaticState.copyFrom(staticState);
+        }
+
+        @Override
+        public void getCurrentLEDState(LEDState desiredState, double timestamp) {
+            desiredState.copyFrom(mStaticState);
+        }
+    }
+}
